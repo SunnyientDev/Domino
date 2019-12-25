ee613d12249b18f9b81333fffd422a57fe84d675
Sunny <38656436+SunnyientDev@users.noreply.github.com>
2019-12-25 20:27:30 +0300

    Update handling.cpp

--- handling.cpp
+++ handling.cpp
@@ -3,25 +3,25 @@
 #include <time.h>
 #include <iostream>
 #include <algorithm>
-  #include <utility>
-  #include "handling.h"
-  #include "game.h"
+   #include <utility>
+   #include "handling.h"
+   #include "game.h"
 using namespace std;
 
-bool Handling::getPlay(){ return play; }
-void Handling::setPlay(bool stan){ play = stan;}
-Domino Handling::getTop(){ return setup.top(); }
+bool Handling :: getPlay () {return play; }
+void Handling :: setPlay (bool stan) {play = stan;}
+Domino Handling :: getTop () {return setup.top (); }
 
-void Handling::SaveMove(int a, int b){
-  Domino block = Domino(a,b); setup.push(block); }
-  
+void Handling :: SaveMove (int a, int b) {
+   Domino block = Domino (a, b); setup.push (block); }
+  
 
-list<Domino> Handling::UndoMove(list<Domino> lista){
-  cout << "Процедура отзыва.\n";
-  Domino block = Domino(0,0);
-  block.A = setup.top().A; // сохранить значение блока
-  block.B = setup.top().B;
-  setup.pop(); //удаляем
-  lista.push_back(block); // дает предыдущему игроку его доминошку
-  return lista;
+list <Domino> Handling :: UndoMove (list <Domino> lista) {
+   cout << "The recall procedure. \ n";
+   Domino block = Domino (0,0);
+   block.A = setup.top (). A; // save the value of the block
+   block.B = setup.top (). B;
+   setup.pop (); // delete
+   lista.push_back (block); // gives the previous player his domino
+   return lista;
 }
